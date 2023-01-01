1fd53345c6d9d21220006f2a3ebfe2fc3c276c92
tanbenwang <13160671810@github.com>
2023-01-01 21:32:50 +0800

    1.修改内容

--- src/main/java/org/tanbenwang/Main.java
+++ src/main/java/org/tanbenwang/Main.java
@@ -5,5 +5,6 @@ import org.tanbenwang.util.concurrent.TestUtil;
 public class Main {
     public static void main(String[] args) {
         TestUtil.test();
+        System.out.println("修改内容！");
     }
 }
